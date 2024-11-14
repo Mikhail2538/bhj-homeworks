87a5320270210b775979ebad3631a80d280baf3d
Mikhail Gr <g_ms@bk.ru>
2024-11-14 20:56:08 +0300

    Первое задание ввыполнено

---
+++ dom/reveal/task.js
@@ -0,0 +1,18 @@
+function isElementInViewport(element) {
+  const rect = element.getBoundingClientRect();
+  return rect.top < window.innerHeight && rect.bottom > 0;
+}
+
+function revealElementsOnScroll() {
+  const revealElements = document.querySelectorAll(".reveal");
+
+  revealElements.forEach((element) => {
+    if (isElementInViewport(element)) {
+      element.classList.add("reveal_active");
+    } else {
+      element.classList.remove("reveal_active");
+    }
+  });
+}
+
+window.addEventListener("scroll", revealElementsOnScroll);
