f234feeed151028bb68c3ad1dddbbff570991aeb
Mikhail Gr <g_ms@bk.ru>
2024-12-08 17:59:46 +0300

    Первое задание выполнено

---
+++ client-state/text-editor/task.js
@@ -0,0 +1,5 @@
+const editor = document.getElementById("editor");
+editor.value = localStorage.getItem("editorContent") || "";
+editor.addEventListener("input", () => {
+  localStorage.setItem("editorContent", editor.value);
+});
